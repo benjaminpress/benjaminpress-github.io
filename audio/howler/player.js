b214a528543626ffd77dd0de164ca7901c412fa8
Sam Press <samuel.press@gmail.com>
2019-08-10 16:36:20 +0100

    fix audio title

--- audio/howler/player.js
+++ audio/howler/player.js
@@ -24,7 +24,7 @@ var Player = function(playlist) {
   this.index = 0;
 
   // Display the title of the first track.
-  track.innerHTML = '1. ' + playlist[0].title;
+  track.innerHTML = '<br />' + playlist[0].title + '<br />';
 
   // Setup the playlist display.
   playlist.forEach(function(song) {
@@ -102,7 +102,7 @@ Player.prototype = {
     sound.play();
 
     // Update the track display.
-    track.innerHTML = (index + 1) + '. ' + data.title;
+    track.innerHTML = '<br />' + data.title + '<br />';
 
     // Show the pause button.
     if (sound.state() === 'loaded') {
